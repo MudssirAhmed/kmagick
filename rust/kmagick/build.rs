44d1f4d5806f54f459868af08ad8d8b5c38bd2ce
Mudssir Ahmed <mudssira01@gmail.com>
2025-09-15 18:10:57 +0530

    build.rs updated

--- rust/kmagick/build.rs
+++ rust/kmagick/build.rs
@@ -2,6 +2,15 @@ use std::env;
 use winres;
 
 fn main() {
+    // Set ImageMagick configuration flags
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-env=MAGICKCORE_HDRI_ENABLE=1");
+    println!("cargo:rustc-env=MAGICKCORE_QUANTUM_DEPTH=16");
+
+    // Add C/C++ compiler flags
+    println!("cargo:rustc-flags=-DMAGICKCORE_HDRI_ENABLE=1");
+    println!("cargo:rustc-flags=-DMAGICKCORE_QUANTUM_DEPTH=16");
+
     let is_windows = env::var("CARGO_CFG_WINDOWS").is_ok();
     // HOST must also be windows to run stamping tool
     if !is_windows || !cfg!(windows) {
